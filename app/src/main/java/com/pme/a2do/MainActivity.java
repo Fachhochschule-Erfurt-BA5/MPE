174c65c82a79796c2477943b0f8797aa4fcf375c
Harti <hamza.harti94@gmail.com>
2020-11-26 10:26:48 +0100

    test
    hg

--- app/src/main/java/com/pme/a2do/MainActivity.java
+++ app/src/main/java/com/pme/a2do/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_view);
 
         /* starts before 1 month from now */
         Calendar startDate = Calendar.getInstance();
