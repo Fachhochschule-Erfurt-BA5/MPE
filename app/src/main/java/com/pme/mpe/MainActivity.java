4709c9c1e3597d623985940581de262336659797
Harti <hamza.harti94@gmail.com>
2021-01-05 11:28:55 +0100

    update

--- app/src/main/java/com/pme/mpe/MainActivity.java
+++ app/src/main/java/com/pme/mpe/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Add a Task", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+       NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
