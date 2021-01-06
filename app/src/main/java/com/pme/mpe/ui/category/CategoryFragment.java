8601833644512b8bd24dc470f4b2ab3b43a9234a
Harti <hamza.harti94@gmail.com>
2021-01-06 12:13:45 +0100

    update

--- app/src/main/java/com/pme/mpe/ui/category/CategoryFragment.java
+++ app/src/main/java/com/pme/mpe/ui/category/CategoryFragment.java
@@ -1,4 +1,4 @@
-package com.pme.mpe.ui.slideshow;
+package com.pme.mpe.ui.category;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.pme.mpe.R;
 
-public class SlideshowFragment extends Fragment {
+public class CategoryFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
+    private CategoryViewModel categoryViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
+        categoryViewModel =
+                new ViewModelProvider(this).get(CategoryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_category, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final TextView textView = root.findViewById(R.id.text_category);
+        categoryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/pme/mpe/ui/category/CategoryViewModel.java
+++ app/src/main/java/com/pme/mpe/ui/category/CategoryViewModel.java
@@ -1,16 +1,16 @@
-package com.pme.mpe.ui.slideshow;
+package com.pme.mpe.ui.category;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class CategoryViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public CategoryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        mText.setValue("Categories will be shown here, once i know how it's done");
     }
 
     public LiveData<String> getText() {
