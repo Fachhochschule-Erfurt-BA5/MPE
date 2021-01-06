0f2f34f77e57ab8730aca4a4c707a8bc01fe834e
Harti <hamza.harti94@gmail.com>
2021-01-06 15:22:34 +0100

    update

--- app/src/main/java/com/pme/mpe/ui/category/CategoryFragment.java
+++ app/src/main/java/com/pme/mpe/ui/category/CategoryFragment.java
@@ -12,7 +12,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+
 import com.pme.mpe.R;
+
+import java.util.List;
 
 public class CategoryFragment extends Fragment {
 
@@ -20,9 +23,41 @@ public class CategoryFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        categoryViewModel =
-                new ViewModelProvider(this).get(CategoryViewModel.class);
+        categoryViewModel = new ViewModelProvider(this).get(CategoryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_category, container, false);
+        final TextView textView = root.findViewById(R.id.category_grid_name);
+        final TextView textView1 = root.findViewById(R.id.category_grid_name2);
+        final TextView textView2 = root.findViewById(R.id.category_grid_name3);
+        final TextView textView3 = root.findViewById(R.id.category_grid_name4);
+        final TextView textView4 = root.findViewById(R.id.category_grid_name5);
+        final TextView textView5 = root.findViewById(R.id.category_grid_text);
+        final TextView nBlock = root.findViewById(R.id.category_grid_block);
+        final TextView nBlock1 = root.findViewById(R.id.category_grid_block2);
+        final TextView nBlock2 = root.findViewById(R.id.category_grid_block3);
+        final TextView nBlock3 = root.findViewById(R.id.category_grid_block4);
+        final TextView nBlock4 = root.findViewById(R.id.category_grid_block5);
+        categoryViewModel.getCategory().observe(getViewLifecycleOwner(), new Observer<List<String>>() {
+            @Override
+            public void onChanged(List<String> strings) {
+                textView.setText(strings.get(0));
+                textView1.setText(strings.get(1));
+                textView2.setText(strings.get(2));
+                textView3.setText(strings.get(3));
+                textView4.setText(strings.get(4));
+                textView5.setText(strings.get(5));
+            }
+        });
+
+        categoryViewModel.getBlock().observe(getViewLifecycleOwner(), new Observer<List<String>>() {
+            @Override
+            public void onChanged(List<String> strings) {
+                nBlock.setText(strings.get(0));
+                nBlock1.setText(strings.get(1));
+                nBlock2.setText(strings.get(2));
+                nBlock3.setText(strings.get(3));
+                nBlock4.setText(strings.get(4));
+            }
+        });
 
         return root;
     }
--- app/src/main/java/com/pme/mpe/ui/category/CategoryViewModel.java
+++ app/src/main/java/com/pme/mpe/ui/category/CategoryViewModel.java
@@ -1,9 +1,49 @@
 package com.pme.mpe.ui.category;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.pme.mpe.model.tasks.Category;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 public class CategoryViewModel extends ViewModel {
 
+    private MutableLiveData<List<String>> gCategoryList;
+    private MutableLiveData<List<String>> gBlockList;
+
+    public CategoryViewModel() {
+        gCategoryList = new MutableLiveData<>();
+        gBlockList = new MutableLiveData<>();
+        List<String> listCategory = new ArrayList<>();
+        List<String> nListBlock = new ArrayList<>();
+
+        listCategory.add("Study");
+        listCategory.add("Fitness");
+        listCategory.add("Cooking");
+        listCategory.add("Work");
+        listCategory.add("Travel");
+        listCategory.add("+");
+        nListBlock.add("25 Blocks");
+        nListBlock.add("5 Blocks");
+        nListBlock.add("10 Blocks");
+        nListBlock.add("35 Blocks");
+        nListBlock.add("15 Blocks");
+        gCategoryList.setValue(listCategory);
+        gBlockList.setValue(nListBlock);
+    }
+
+    public LiveData<List<String>> getCategory() {
+        return gCategoryList;
+    }
+    public LiveData<List<String>> getBlock() {
+        return gBlockList;
+    }
 }
