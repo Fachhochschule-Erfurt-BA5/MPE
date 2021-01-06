bfda3680fa1239bbe3490124f15bdbf5a1b0743b
Harti <hamza.harti94@gmail.com>
2021-01-06 15:37:53 +0100

    Fragment : categories , Blocks

--- app/src/main/java/com/pme/mpe/ui/block/BlockFragment.java
+++ app/src/main/java/com/pme/mpe/ui/block/BlockFragment.java
@@ -14,15 +14,42 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.pme.mpe.R;
 
+import java.util.List;
+
 public class BlockFragment extends Fragment {
 
     private BlockViewModel blockViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        blockViewModel =
-                new ViewModelProvider(this).get(BlockViewModel.class);
+        blockViewModel = new ViewModelProvider(this).get(BlockViewModel.class);
         View root = inflater.inflate(R.layout.fragment_block, container, false);
+        final TextView blockText = root.findViewById(R.id.block_name);
+        final TextView blockText2 = root.findViewById(R.id.block_name2);
+        final TextView blockText3 = root.findViewById(R.id.block_name3);
+        final TextView blockText4 = root.findViewById(R.id.add_category);
+        final TextView taskText = root.findViewById(R.id.block_task_number);
+        final TextView taskText2 = root.findViewById(R.id.block_task_number2);
+        final TextView taskText3 = root.findViewById(R.id.block_task_number3);
+        blockViewModel.getBlockCategory().observe(getViewLifecycleOwner(), new Observer<List<String>>() {
+            @Override
+            public void onChanged(List<String> strings) {
+                blockText.setText(strings.get(0));
+                blockText2.setText(strings.get(1));
+                blockText3.setText(strings.get(2));
+                blockText4.setText(strings.get(3));
+
+            }
+        });
+
+        blockViewModel.getNTask().observe(getViewLifecycleOwner(), new Observer<List<String>>() {
+            @Override
+            public void onChanged(List<String> strings) {
+                taskText.setText(strings.get(0));
+                taskText2.setText(strings.get(1));
+                taskText3.setText(strings.get(2));
+            }
+        });
         return root;
     }
 }
--- app/src/main/java/com/pme/mpe/ui/block/BlockViewModel.java
+++ app/src/main/java/com/pme/mpe/ui/block/BlockViewModel.java
@@ -4,6 +4,34 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class BlockViewModel extends ViewModel {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BlockViewModel extends ViewModel {
+    private MutableLiveData<List<String>> gBlockCategoyList;
+    private MutableLiveData<List<String>> gTaskList;
+
+    public BlockViewModel() {
+        gBlockCategoyList = new MutableLiveData<>();
+        gTaskList = new MutableLiveData<>();
+        List<String> listBlock = new ArrayList<>();
+        List<String> nListTask = new ArrayList<>();
+
+        listBlock.add("Block 1");
+        listBlock.add("Block 2");
+        listBlock.add("Block 3");
+        listBlock.add("+");
+        nListTask.add("25 Tasks");
+        nListTask.add("5 Tasks");
+        nListTask.add("10 Tasks");
+        gBlockCategoyList.setValue(listBlock);
+        gTaskList.setValue(nListTask);
+    }
+
+    public LiveData<List<String>> getBlockCategory() {
+        return gBlockCategoyList;
+    }
+    public LiveData<List<String>> getNTask() {
+        return gTaskList;
+    }
 }
