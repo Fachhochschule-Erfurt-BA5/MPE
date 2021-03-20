71b521e79e7c4f2b4ca0368f0d75a86491976d45
Harti <hamza.harti94@gmail.com>
2021-03-20 21:01:11 +0100

    home Fragment

--- app/src/main/java/com/pme/mpe/ui/block/BlockAdapter.java
+++ app/src/main/java/com/pme/mpe/ui/block/BlockAdapter.java
@@ -2,6 +2,7 @@ package com.pme.mpe.ui.block;
 
 import android.app.Application;
 import android.graphics.Color;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,7 @@ import androidx.lifecycle.ViewModelStoreOwner;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.card.MaterialCardView;
 import com.pme.mpe.R;
 import com.pme.mpe.activities.TaskActivity.NewTaskActivityViewModel;
 import com.pme.mpe.model.tasks.CategoryBlock;
@@ -56,6 +58,23 @@ public class BlockAdapter extends RecyclerView.Adapter<BlockAdapter.BlockViewHol
         blockViewHolder.rvTaskItem.setLayoutManager(layoutManager);
         blockViewHolder.rvTaskItem.setAdapter(taskAdapter);
         blockViewHolder.rvTaskItem.setRecycledViewPool(viewPool);
+
+        blockViewHolder.blockCard.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                blockViewHolder.blockCard.getLayoutParams().height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                blockViewHolder.blockCard.requestLayout();
+                        return true;
+            }
+        });
+
+        blockViewHolder.blockCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blockViewHolder.blockCard.getLayoutParams().height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 185, v.getResources().getDisplayMetrics());
+                blockViewHolder.blockCard.requestLayout();
+            }
+        });
     }
 
     @Override
@@ -68,6 +87,7 @@ public class BlockAdapter extends RecyclerView.Adapter<BlockAdapter.BlockViewHol
         private RecyclerView rvTaskItem;
         private LinearLayout blockItem;
         private TextView timeTask;
+        private MaterialCardView blockCard;
 
         BlockViewHolder(View itemView) {
             super(itemView);
@@ -75,6 +95,7 @@ public class BlockAdapter extends RecyclerView.Adapter<BlockAdapter.BlockViewHol
             rvTaskItem = itemView.findViewById(R.id.rv_task_item);
             blockItem = itemView.findViewById(R.id.block_item_layout);
             timeTask = itemView.findViewById(R.id.block_time_item);
+            blockCard = itemView.findViewById(R.id.block_card);
         }
     }
     public List<Task> assignTasks(CategoryBlock block, List<Task> taskList){
