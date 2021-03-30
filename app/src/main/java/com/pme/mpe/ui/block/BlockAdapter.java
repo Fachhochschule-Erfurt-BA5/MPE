63e42f328a0a436ce0a4b4b1f00cc23bade20f1d
Harti <hamza.harti94@gmail.com>
2021-03-30 21:19:03 +0200

    Delete Task in Progress

--- app/src/main/java/com/pme/mpe/ui/block/BlockAdapter.java
+++ app/src/main/java/com/pme/mpe/ui/block/BlockAdapter.java
@@ -48,16 +48,25 @@ public class BlockAdapter extends RecyclerView.Adapter<BlockAdapter.BlockViewHol
     public void onBindViewHolder(@NonNull BlockViewHolder blockViewHolder, int i) {
         CategoryBlock block = BlockList.get(i);
         blockViewHolder.blockTitle.setText(block.getTitle());
-        blockViewHolder.timeBlock.setText(block.getStartTimeHour()+"h - "+block.getEndTimeHour()+"h");
+        blockViewHolder.timeBlock.setText(block.getStartTimeHour() + "h - " + block.getEndTimeHour() + "h");
         blockViewHolder.blockTitle.setTextColor(Color.parseColor(taskViewModel.getCategoryWithID(block.CB_CategoryId).getLetterColor()));
         blockViewHolder.timeBlock.setTextColor(Color.parseColor(taskViewModel.getCategoryWithID(block.CB_CategoryId).getLetterColor()));
+        blockViewHolder.blockTasks.setTextColor(Color.parseColor(taskViewModel.getCategoryWithID(block.CB_CategoryId).getLetterColor()));
+        blockViewHolder.blockNoTask.setTextColor(Color.parseColor(taskViewModel.getCategoryWithID(block.CB_CategoryId).getLetterColor()));
         blockViewHolder.blockCard.setCardBackgroundColor(Color.parseColor(taskViewModel.getCategoryWithID(block.CB_CategoryId).getColor()));
-
         LinearLayoutManager layoutManager = new LinearLayoutManager(blockViewHolder.rvTaskItem.getContext(), LinearLayoutManager.VERTICAL, false);
 
-       layoutManager.setInitialPrefetchItemCount(assignTasks(block,taskList).size());
-       TaskAdapter taskAdapter = new TaskAdapter(assignTasks(block,taskList), tasksPackageRepository, taskViewModel);
+        layoutManager.setInitialPrefetchItemCount(assignTasks(block, taskList).size());
+        blockViewHolder.blockTasks.setText(assignTasks(block, taskList).size()+" Tasks");
+        TaskAdapter taskAdapter = new TaskAdapter(assignTasks(block, taskList), tasksPackageRepository, taskViewModel);
+        if (assignTasks(block, taskList).size() == 0) {
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 35, blockViewHolder.itemView.getResources().getDisplayMetrics()));
+            layoutParams.setMargins(0, 30, 0, 0);
 
+            blockViewHolder.blockNoTask.setLayoutParams(layoutParams);
+            blockViewHolder.blockNoTask.requestLayout();
+        }
 
         blockViewHolder.rvTaskItem.setLayoutManager(layoutManager);
         blockViewHolder.rvTaskItem.setAdapter(taskAdapter);
@@ -68,14 +77,14 @@ public class BlockAdapter extends RecyclerView.Adapter<BlockAdapter.BlockViewHol
             public boolean onLongClick(View v) {
                 blockViewHolder.blockCard.getLayoutParams().height = LinearLayout.LayoutParams.WRAP_CONTENT;
                 blockViewHolder.blockCard.requestLayout();
-                        return true;
+                return true;
             }
         });
 
         blockViewHolder.blockCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                blockViewHolder.blockCard.getLayoutParams().height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 185, v.getResources().getDisplayMetrics());
+                blockViewHolder.blockCard.getLayoutParams().height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 127, v.getResources().getDisplayMetrics());
                 blockViewHolder.blockCard.requestLayout();
             }
         });
@@ -91,7 +100,9 @@ public class BlockAdapter extends RecyclerView.Adapter<BlockAdapter.BlockViewHol
         private RecyclerView rvTaskItem;
         private LinearLayout blockItem;
         private TextView timeBlock;
+        private TextView blockNoTask;
         private MaterialCardView blockCard;
+        private TextView blockTasks;
 
         BlockViewHolder(View itemView) {
             super(itemView);
@@ -99,13 +110,16 @@ public class BlockAdapter extends RecyclerView.Adapter<BlockAdapter.BlockViewHol
             rvTaskItem = itemView.findViewById(R.id.rv_task_item);
             blockItem = itemView.findViewById(R.id.block_item_layout);
             timeBlock = itemView.findViewById(R.id.block_time_item);
+            blockNoTask = itemView.findViewById(R.id.block_time_no_tasks);
             blockCard = itemView.findViewById(R.id.block_card);
+            blockTasks = itemView.findViewById(R.id.block_tasks_item);
         }
     }
-    public List<Task> assignTasks(CategoryBlock block, List<Task> taskList){
+
+    public List<Task> assignTasks(CategoryBlock block, List<Task> taskList) {
         List<Task> selectedTasks = new ArrayList<>();
-        for(int i = 0; i<= taskList.size()-1; i++){
-            if (taskList.get(i).getT_categoryBlockId()==block.getCatBlockId()){
+        for (int i = 0; i <= taskList.size() - 1; i++) {
+            if (taskList.get(i).getT_categoryBlockId() == block.getCatBlockId()) {
                 selectedTasks.add(taskList.get(i));
             }
 
